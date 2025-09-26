4025f786daaa7a8e1dfdb54fc35372cb14398e24
dineshajay367-glitch <dineshajay367@gmail.com>
2025-09-26 19:07:50 +0530

    Add files via upload

---
+++ singleton/src/singleton/Singleton.java
@@ -0,0 +1,27 @@
+package singleton;
+
+public class Singleton {
+	private static volatile Singleton instance;
+	private Singleton()
+	{
+		System.out.println("Instance is Created");
+	}
+	public static Singleton getInstance()
+	{
+	if(instance==null)
+	{
+		synchronized(Singleton.class)
+		{
+			if(instance==null)
+			{
+				instance=new Singleton();
+			}
+		}
+		//instance=new Singleton();
+		
+	}
+	return instance;
+	}
+	
+
+}
